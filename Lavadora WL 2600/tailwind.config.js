da43111006adaa5323d0ec26b60cf8477664de0f
SrDouglax <Meioameio8686@gmail.com>
2023-12-20 13:36:53 -0300

    feat: some changes

--- Lavadora WL 2600/tailwind.config.js
+++ Lavadora WL 2600/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "zinc-400": "#434343",
+      },
+      fontSize: {
+        base: "18px",
+      },
+    },
     fontWeight: {},
   },
   plugins: [],
