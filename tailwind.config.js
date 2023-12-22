e356a84c1faa7096442c2730c0045bcfd5e9ce73
SrDouglax <Meioameio8686@gmail.com>
2023-12-22 09:09:10 -0300

    refactor/feat: some improvements

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "zinc-400": "#434343",
+      },
+      fontSize: {
+        title: "16pt", // Adicione tamanhos de fonte personalizados
+        "title-md": "33px",
+        body: "12pt",
+        "body-md": "18px",
+      },
+    },
+    fontWeight: {},
+  },
+  plugins: [],
+};
